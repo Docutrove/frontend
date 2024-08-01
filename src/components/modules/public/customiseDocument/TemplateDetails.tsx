4d40a0fc1eee172788a2afd836f130ad5063633b
Matthew Idungafa <mattidungafa@gmail.com>
2024-08-01 14:13:05 +0100

    updates: revert from html to previewHtml on templateDetails page

--- src/components/modules/public/customiseDocument/TemplateDetails.tsx
+++ src/components/modules/public/customiseDocument/TemplateDetails.tsx
@@ -75,7 +75,7 @@ export default function TemplateDetails() {
   useEffect(() => {
     if (localTemplate) {
       const fields = localTemplate.configuration.formConfig.modules
-      const html = localTemplate.configuration.html //changed this from previewHtml
+      const html = localTemplate.configuration.previewHtml //changed this from previewHtml
       const completeHtml = localTemplate.configuration.html
 
       const initialFormData: { [key: string]: string | string[] } = {}
